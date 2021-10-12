45d53226199a0a27f6a0a9e583d7dec27f2a0eb4
Anhhhuy16666 <88813542+Anhhhuy16666@users.noreply.github.com>
2021-10-12 11:59:43 +0700

    first commit

---
+++ BTThang2/Controllers/IdentityController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BTThang2.Models;
+using System.Web.Security;
+
+namespace BTThang2.Controllers
+{
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        public ActionResult Register()// Cho phep hien thi danh sach quyen co the chon
+        {
+            string[] Roles = System.Web.Security.Roles.GetAllRoles();
+            List<String> RolesList = new List<string>();
+            foreach (var r in Roles)
+            {
+                if (HttpContext.User.IsInRole("Admin"))
+                    RolesList.Add(r);
+                else
+                {
+                    if(r!="Admin")
+                    {
+                        RolesList.Add(r);
+                    }
+                }
+            }
+            UsersModel usermodel = new UsersModel();
+            usermodel.Roles = RolesList;
+            return View(usermodel);
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Register(UsersModel usermodel)
+        {
+            MembershipCreateStatus memCreSta;
+            Membership.CreateUser(usermodel.usernname,usermodel.password,usermodel.email,
+                usermodel.passwordquestion,usermodel.passwordanswer,usermodel.isapproved,out memCreSta);
+            if(memCreSta==MembershipCreateStatus.Success)
+            {
+                Roles.AddUsersToRole(new[] { usermodel.usernname }, usermodel.Roles[0]);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        // GET: Identity
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BTThang2/Models/MemberShip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTThang2.Models
+{
+    public class UsersModel
+    {
+        public string usernname { set; get; }
+        public string password { set; get; }
+        public string email { set; get; }
+        public string passwordquestion { set; get; }
+        public string passwordanswer { set; get; }
+        public bool isapproved { set; get; }
+        public  List<string> Roles { set; get; }
+    }
+    public  class RolesModel
+    {
+        public string rolename { set; get; }
+    }
+}
